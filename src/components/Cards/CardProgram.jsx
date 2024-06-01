25c21e70e432c9160c8913851ab55d2ae5ba19d6
Deni210303 <alqastha@gmail.com>
2024-06-01 20:54:57 +0700

    add faq & program publikasi

---
+++ src/components/Cards/CardProgram.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { programData } from "../../data/programData";
+import { useNavigate } from 'react-router-dom';
+
+const CardProgram = ({ limit }) => {
+  const navigate = useNavigate();
+
+  const handleCardClick = (id) => {
+    navigate('/laporan/'+id);
+  };
+
+  return (
+    <>
+      {programData.slice(0, limit).map((data, index) => {
+        return (
+          <div
+            className="relative w-full sm:w-[345px] h-[350px] rounded overflow-hidden shadow-lg cursor-pointer"
+            key={index}
+            onClick={() => handleCardClick(data.id)}
+          >
+            <img className="w-full h-56 object-cover" src={data.image} alt="Placeholder" />
+            <div className="absolute px-6 py-4 bg-white rounded-tr-3xl top-1/2">
+              <div className="font-bold text-xl mt-3">{data.title}</div>
+              <p className="text-gray-700 text-base pt-7">{data.description}</p>
+            </div>
+          </div>
+        );
+      })}
+    </>
+  );
+};
+
+export default CardProgram;
